2648e7fd893d7623a861b88906885e6bc859e559
HEMANTH2501 <gobbillahemanth@gmail.com>
2025-12-24 12:25:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -85,3 +85,13 @@ for i in range(n):
         else:
              print(" ",end=" ")
     print()
+
+#hallow pyramid
+n=int(input())
+for i in range(n):
+    for j in range(i+1):
+        if i==n-1 or j==0 or i==j:
+           print("*",end=" ")
+        else:
+             print(" ",end=" ")
+    print()
