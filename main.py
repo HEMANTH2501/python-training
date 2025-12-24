e7d2ecf4f8805376f24202e68c3a14faf2c26f09
HEMANTH2501 <gobbillahemanth@gmail.com>
2025-12-24 12:02:54 +0530

    Implement FizzBuzz and various patterns in main.py
    Added multiple patterns and FizzBuzz implementation.

--- main.py
+++ main.py
@@ -8,3 +8,72 @@ for i in range(1,x+1):
         print("fizz")
     else:
         print(i)
+
+#version2
+x=int(input())
+for i in range(1,x+1):
+    if(i%3==0):
+        if(i%5==0):
+            print("fizz buzz")
+    elif(i%5==0):
+        print("buzz")
+    elif(i%3==0):
+        print("fizz")
+    else:
+        print(i)
+
+#square pattern
+x=int(input())
+for i in range(x):
+    for j in range(x):
+        print("*",end=" ")
+    print()    
+
+#sequence pattern
+x=int(input())
+for i in range(x):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()    
+
+#number pattern
+x=int(input())
+for i in range(x):
+    for j in range(i+1):
+        print(j,end=" ")
+    print()    
+
+
+#pyramid pattern 
+x=int(input())
+for i in range(x):
+    for j in range(x-i-1):
+        print(" ",end="")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()    
+
+#diamond pattern
+x=int(input())
+for i in range(x):
+    for j in range(x-i-1):
+        print(" ",end="")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+for i in range(x-2,-1,-1):
+    for j in range(x-i-1):
+        print(" ",end="")
+    for j in range(i+1):
+        print("*",end=" ")
+    print()   
+    
+#odd pattern
+x=int(input())
+for i in range(x):
+    for j in range(x-i-1):
+        print(" ",end=" ")
+    for j in range(2*i+1):
+        print("*",end=" ")
+    print()
+        
