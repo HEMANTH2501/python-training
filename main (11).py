937f9c630f4d84f96db981cec1d84f85734784ca
HEMANTH2501 <gobbillahemanth@gmail.com>
2025-12-29 10:10:02 +0530

    Add files via upload

---
+++ main (11).py
@@ -0,0 +1,21 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        c=0
+        candidate=0
+        for num in nums:
+            if c==0:
+                candidate=num
+                c+=1
+            elif candidate==num:
+                c+=1
+            else:
+                c-=1
+        return candidate
