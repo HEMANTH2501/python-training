3f2f6ca54b6885350b9d844af5d98f48cdcbdf26
HEMANTH2501 <gobbillahemanth@gmail.com>
2025-12-30 14:15:43 +0530

    Update main (5).py

--- main (5).py
+++ main (5).py
@@ -1,18 +1,26 @@
 #LEETCODE PROBLEM 229
-def majorityElementII(nums):
-    n=len(nums)
-    d={}
-    out=[]
-    for i in range(len(nums)):
-        if nums[i] in d:
-            d[nums[i]]+=1
-        else:
-            d[nums[i]]=1
-       
-    for key,value in d.items():
-        if value>n//3:
-            out.append(key)
-    return out
-        
-arr=list(map(int,input().split()))
-print(majorityElementII(arr))      
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        c1=0
+        c2=0
+        num1=num2=None
+        for num in nums:
+            if num==num1:
+                c1+=1
+            elif num==num2:
+                c2+=1
+            elif c1==0:
+                num1=num
+                c1=1
+            elif c2==0:
+                num2=num
+                c2=1
+            else:
+                c1-=1
+                c2-=1
+        k=[]
+        if nums.count(num1)>len(nums)//3:
+            k.append(num1)
+        if nums.count(num2)>len(nums)//3:
+             k.append(num2)
+        return k
